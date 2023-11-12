0915a9152d05983ffd6b4c4d4970ff4b77257fce
bolatu <3057339791@qq.com>
2023-11-12 18:42:52 +0800

    hotfix commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git4r!");
         System.out.println("hello git6!");
+        System.out.println("hot-fix test!");
     }
 }
