76b6887ad00473483ef3ece74b0b1de33708e395
bolatu <3057339791@qq.com>
2023-11-13 09:55:30 +0800

    master commit4

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -13,5 +13,8 @@ public class GitTest {
         System.out.println("hot-fix test3!");
         System.out.println("hot-fix test4!");
         System.out.println("pull tesr");
+        System.out.println("pull tesr1");
+        System.out.println("pull tesr2");
+        System.out.println("pull tesr3");
     }
 }
